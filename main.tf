32c812d129a15063f916a9efca567bb2be2eeff0
Travis N. Thomsen <travis@linuxacademy.com>
2019-04-11 16:41:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "docker_network" "public_bridge_network" {
 }
 
 resource "docker_network" "private_bridge_network" {
-  name     = "mysql_internal"
+  name     = "ghost_mysql_internal"
   driver   = "overlay"
   internal = true
 }
